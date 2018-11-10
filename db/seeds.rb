8acb2a6d4b73b5f1ea9c52b06de61118ecea42da
vegitalander <vegitalander@gmail.com>
2018-11-10 02:34:07 -0800

    Remove seeds from git tracking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'potter', first_name: 'Michael', last_name: 'Toews', email_address: 'potter@beeflord.com', password: 'password123')
+User.create(username: 'beeflord', first_name: 'Julie', last_name: 'Tran', email_address: 'beeflord@beeflord.com', password: 'password123')
+User.create(username: 'jerky_jester', first_name: 'Ryan', last_name: 'Erikson', email_address: 'jerky_jester@beeflord.com', password: 'password123')
+
